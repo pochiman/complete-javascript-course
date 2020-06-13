0e7ace76cad49a4844f4eb7af40eec171cbdfc07
pochiman <pochiman@yahoo.com>
2020-06-12 23:42:21 -0400

    initial commit

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -58,10 +58,12 @@ console.log(firstName + ' ' + lastName);
 */
 var year, yearJohn, yearMark;
 now = 2018;
+ageJohn = 28;
+ageMark = 33;
 
 // Math operators
-yearJohn = now - 28;
-yearMark = now - 33;
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
 
 console.log(yearJohn);
 
@@ -71,3 +73,13 @@ console.log(now / 10);
 
 
 // Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
