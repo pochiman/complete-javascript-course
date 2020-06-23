02065e503c259de46f1c2ccade500b6311c8a046
pochiman <pochiman@yahoo.com>
2020-06-22 23:37:43 -0400

    initial commit

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -216,7 +216,7 @@ if (age < 13) {
 /*****************************
 * The Ternary Operator and Switch Statements
 */
-
+/*
 var firstName = 'John';
 var age = 14;
 
@@ -273,7 +273,7 @@ switch (true) {
 
 // falsy values: undefined, null, 0, '', NaN
 // truthy values: NOT falsy values
-
+/*
 var height;
 
 height = 23;
@@ -288,3 +288,23 @@ if (height || height === 0) {
 if (height === '23') {
   console.log('The == operator does type coercion!');
 }
+*/
+
+
+
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winner, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
