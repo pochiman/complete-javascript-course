fedb91a780040d5e0d79a00acc442aba5b2b2ae7
pochiman <pochiman@yahoo.com>
2020-06-23 22:52:51 -0400

    initial commit

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -308,3 +308,28 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 
 GOOD LUCK 😀
 */
+
+var scoreJohn = (89 + 120 + 103) / 3;
+var scoreMike = (116 + 94 + 123) / 3;
+var scoreMary = (97 + 134 + 105) / 3;
+console.log(scoreJohn, scoreMike, scoreMary);
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+  console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+  console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+  console.log('Mary\'s team wins with ' + scoreMary + ' points');
+} else {
+  console.log('There is a draw');
+}
+
+
+
+/*if (scoreJohn > scoreMike) {
+  console.log('John\'s team wins with ' + scoreJohn + ' points');
+} else if (scoreMike > scoreJohn) {
+  console.log('Mike\'s team wins with ' + scoreMike + ' points');
+} else {
+  console.log('There is a draw');
+}*/
