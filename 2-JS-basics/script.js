ef3080de6a47b8d03ae8ef9df26557cd74703766
pochiman <pochiman@yahoo.com>
2020-06-24 23:00:44 -0400

    initial commit

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -308,7 +308,7 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 
 GOOD LUCK 😀
 */
-
+/*
 var scoreJohn = (89 + 120 + 103) / 3;
 var scoreMike = (116 + 94 + 123) / 3;
 var scoreMary = (97 + 134 + 105) / 3;
@@ -323,7 +323,7 @@ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
 } else {
   console.log('There is a draw');
 }
-
+*/
 
 
 /*if (scoreJohn > scoreMike) {
@@ -333,3 +333,35 @@ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
 } else {
   console.log('There is a draw');
 }*/
+
+
+
+/*****************************
+* Functions
+*/
+
+function calculateAge(birthYear) {
+  return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName) {
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(firstName + ' retires in ' + retirement + ' years.');
+  } else {
+    console.log(firstName + ' is already retired.')
+  }
+  
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
