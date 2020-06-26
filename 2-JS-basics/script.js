fa9ad6f56d09fb9de2495e402f6cf3751af8a20b
pochiman <pochiman@yahoo.com>
2020-06-25 23:26:59 -0400

    initial commit

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -339,7 +339,7 @@ if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
 /*****************************
 * Functions
 */
-
+/*
 function calculateAge(birthYear) {
   return 2018 - birthYear;
 }
@@ -365,3 +365,31 @@ function yearsUntilRetirement(year, firstName) {
 yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1969, 'Jane');
+*/
+
+
+
+/*****************************
+* Function Statements and Expressions
+*/
+
+// Function declaration
+// function whatDoYouDo(job, firstName) {}
+
+// Function expression
+var whatDoYouDo = function(job, firstName) {
+  switch(job) {
+    case 'teacher':
+      return firstName + ' teaches kids how to code.';
+    case 'driver':
+      return firstName + ' drives a cab in Lisbon.';
+    case 'designer':
+      return firstName + ' designs beautiful websites.';
+    default:
+      return firstName + ' does something else';
+  }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
