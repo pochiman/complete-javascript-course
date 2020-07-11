361525d388d58f852597c7a1e0270c0fdd76100e
pochiman <pochiman@yahoo.com>
2020-07-10 23:06:04 -0400

    initial commit

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -610,6 +610,19 @@ while (i < john.length) {
 
 // continue and break statements
 var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
 for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') continue;
+  console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') break;
+  console.log(john[i]);
+}
+
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
   console.log(john[i]);
 }
